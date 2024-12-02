1bb44d22ac5d1c8a73d9089412840b81c369dba5
Serg <martynenko1981@gmail.com>
2024-12-02 10:37:38 +0300

    feat: Task-5 compleated V2

--- src/main/java/hiber/dao/UserDao.java
+++ src/main/java/hiber/dao/UserDao.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 
 import java.util.List;
@@ -7,4 +8,12 @@ import java.util.List;
 public interface UserDao {
    void add(User user);
    List<User> listUsers();
+
+   void update(User user);
+
+   void add(Car car);
+
+   List<Car> listCars();
+
+   User getUserByCar(String model, int series);
 }
--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -1,4 +1,61 @@
 package hiber.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne
+    //@MapsId
+    @JoinColumn(name = "user_id")//, nullable = true) // Внешний ключ, указывающий на User
+    private User user;
+
+
+    public Car() {
+    }
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
